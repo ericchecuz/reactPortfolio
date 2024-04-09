adc1fb5c170ac4e84649e2280f3c3cd73190bf9f
ericchecuz <eric.checuz96@gmail.com>
2024-04-09 12:46:53 +0200

    fix login css & add spinner login to loading wip form login w/ react library

--- src/pages/loginPage.js
+++ src/pages/loginPage.js
@@ -1,13 +1,21 @@
 // src/pages/LoginPage.js
 
-import React, { useState } from 'react';
+import React, { useState , useEffect} from 'react';
 import { auth } from '../firebase/firebaseConfig';
 import './styleLogin.css';
 
 function LoginPage() {
   const [email, setEmail] = useState('');
+  const [loading, setLoading] = useState(true);
   const [password, setPassword] = useState('');
 
+  useEffect(() => {
+    // Simula il caricamento o attendi il caricamento di eventuali risorse
+    const timer = setTimeout(() => setLoading(false), 5000); // 2 secondi per l'esempio
+    return () => clearTimeout(timer);
+  }, []);
+
+  
   const signIn = async (e) => {
     e.preventDefault(); // Previeni il comportamento di submit di default
 
@@ -21,26 +29,35 @@ function LoginPage() {
   };
 
   return (
-    <div className='container-login'>
-      <div className='container-form-login'>
-      <form onSubmit={signIn}>
-        <input
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          placeholder="Email"
-          required
-        />
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          placeholder="Password"
-          required
-        />
-        <button type="submit">Login</button>
-        </form>
+    <div className="background-image">
+      {loading ? (
+        <div className='container-spinner'>
+          <div className="loading-animation"></div>
+          </div>  // Mostra l'animazione di caricamento
+      ) : (
+        // Una volta completato il caricamento, mostra il form di login
+        <div className='container-login fade-in'>
+          <div className='container-form-login'>
+            <form className='form-login' onSubmit={signIn}>
+              <input
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                placeholder="Email"
+                required
+              />
+              <input
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="Password"
+                required
+              />
+              <button type="submit">Login</button>
+            </form>
+          </div>
         </div>
+      )}
     </div>
   );
 }
