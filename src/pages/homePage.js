dbc57e8488f7bcb6d574a35068ea6ff5ffaf45bf
ericchecuz <eric.checuz96@gmail.com>
2024-04-16 11:16:04 +0200

    fix homepage & wip tag/contacts

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -12,19 +12,29 @@ function HomePage() {
                     <div className='container-title2'>DEVICE</div>
                     </div>
                 <div className='container-link'>
-                <a > ABOUT </a> 
-                <a > WORKS</a>
-                <a > CONTACT</a>
+                <a className='link' > ABOUT </a> 
+                <a className='link' > WORKS</a>
+                <a className='link'> CONTACT</a>
                 <div className='container-theme'> <span class="material-symbols-outlined">dark_mode</span></div>
             </div>
             </header>
             <div className='container-top'>
                 <div className='container-titles'>
-                    <div className='container-title'>ERIC CHECUZ </div>
+                    <div className='container-title'>Eric Checuz </div>
                     <div className='container-subtitles'>
-                    <div className='container-subtitle1'><h2><span>Front-End</span>Developer</h2></div>
+                    <div className='container-subtitle1'><h2 className='subtitle3'><span className='subtitle4'>Front-End </span> Developer</h2></div>
                         
-                        </div>
+                    </div>
+                    <div className='container-tag'>
+                    <div className='tag'>JavaScript</div>
+                    <div className='tag'>TypeScript</div>
+                    <div className='tag'>Angular</div>
+                    <div className='tag'>React</div>
+                    <div className='tag'>Java</div>
+                    <div className='tag'>Springboot</div>
+                    <div className='tag'>Python</div>
+                    <div className='tag'>SQL</div>
+                    </div>
                 </div>
 
                 <div className='container-img-top'> <img className='img-top'></img> </div>
