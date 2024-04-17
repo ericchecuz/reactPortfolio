3926799b3f18f9d91b1ac82617f81acc44432c9e
ericchecuz <eric.checuz96@gmail.com>
2024-04-17 12:46:02 +0200

    scrollbar style fix

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -1,9 +1,9 @@
-import React, { useState , useEffect} from 'react';
-import { auth } from '../firebase/firebaseConfig';
-import './styleHome.css';
+import React, { useState, useEffect } from "react";
+import { auth } from "../firebase/firebaseConfig";
+import "./styleHome.css";
 
 function HomePage() {
-    const [transform, setTransform] = useState('');
+  const [transform, setTransform] = useState("");
 
   const handleMouseMove = (event) => {
     const { clientX, clientY, currentTarget } = event;
@@ -17,77 +17,78 @@ function HomePage() {
   };
 
   const handleMouseLeave = () => {
-    setTransform(''); // Resetta la trasformazione quando il mouse lascia il contenitore
-    };
-    
-    return (
-        <div>
-        <div className="main-container">
-            <header className='header'>
-                <div className='logo-title'>
-                    <div className='container-logo'></div>
-                    <div className='container-title1'>ERIC</div>&nbsp;
-                    <div className='container-title2'>DEVICE</div>
-                    </div>
-                <div className='container-link'>
-                <a className='link' > ABOUT </a> 
-                <a className='link' > WORKS</a>
-                <a className='link'> CONTACT</a>
-                <div className='container-theme'> <span class="material-symbols-outlined">dark_mode</span></div>
-            </div>
-            </header>
-            <div className='container-top'>
-                <div className='container-titles'>
-                    <div className='container-title'>Eric Checuz </div>
-                    <div className='container-subtitles'>
-                    <div className='container-subtitle1'><h2 className='subtitle3'><span className='subtitle4'>Front-End </span> Developer</h2></div>
-                        
-                    </div>
-                    <div className='container-tag'>
-                    <div className='tag'>JavaScript</div>
-                    <div className='tag'>TypeScript</div>
-                    <div className='tag'>Angular</div>
-                    <div className='tag'>React</div>
-                    <div className='tag'>Java</div>
-                    <div className='tag'>Springboot</div>
-                    <div className='tag'>Python</div>
-                        <div className='tag'>SQL</div>
-                        <div className='tag'>Figma</div>
-                    
-                    </div>
-                </div>
+    setTransform(""); // Resetta la trasformazione quando il mouse lascia il contenitore
+  };
 
-                <div 
-                    className="container-img-top" 
-                    onMouseMove={handleMouseMove} 
-                    onMouseLeave={handleMouseLeave}
-                    style={{ transform: transform, transition: 'transform 0.5s ease' }}>
-                    <img className='img-top'></img>
-                </div>
-                </div>
-                <div className='container-contacts'>
-                    <div className='contacts'>
-                        <div className='container-icons'>
-                        <div className='facebook'>
-                    </div>
-                     <div className='instagram'>
-                            </div>
-                            <div className='dividing-line'>
-                    </div>
-                     <div className='linkedin'>
-                    </div>
-                     <div className='telegram'>
-                    </div>
-                        </div>
-                        </div>
+  return (
+    <div>
+      <div className="main-container">
+        <header className="header">
+          <div className="logo-title">
+            <div className="container-logo"></div>
+            <div className="container-title1">ERIC</div>&nbsp;
+            <div className="container-title2">DEVICE</div>
+          </div>
+          <div className="container-link">
+            <a className="link"> ABOUT </a>
+            <a className="link"> WORKS</a>
+            <a className="link"> CONTACT</a>
+            <div className="container-theme">
+              {" "}
+              <span class="material-symbols-outlined">dark_mode</span>
             </div>
+          </div>
+        </header>
+        <div className="container-top">
+          <div className="container-titles">
+            <div className="container-title">Eric Checuz </div>
+            <div className="container-subtitles">
+              <div className="container-subtitle1">
+                <h2 className="subtitle3">
+                  <span className="subtitle4">Front-End </span> Developer
+                </h2>
+              </div>
+            </div>
+            <div className="container-tag">
+              <div className="tag">JavaScript</div>
+              <div className="tag">TypeScript</div>
+              <div className="tag">Angular</div>
+              <div className="tag">React</div>
+              <div className="tag">Java</div>
+              <div className="tag">Springboot</div>
+              <div className="tag">Python</div>
+              <div className="tag">SQL</div>
+              <div className="tag">Figma</div>
+            </div>
+          </div>
+
+          <div
+            className="container-img-top"
+            onMouseMove={handleMouseMove}
+            onMouseLeave={handleMouseLeave}
+            style={{
+              transform: transform,
+              transition: "transform 0.5s ease",
+            }}
+          >
+            <img className="img-top"></img>
+          </div>
         </div>
-            <div className='container-img2'></div>
+        <div className="container-contacts">
+          <div className="contacts">
+            <div className="container-icons">
+              <div className="facebook"></div>
+              <div className="instagram"></div>
+              <div className="dividing-line"></div>
+              <div className="linkedin"></div>
+              <div className="telegram"></div>
             </div>
-            
-            
-            
-    )
- }
+          </div>
+        </div>
+      </div>
+      <div className="container-img2"></div>
+    </div>
+  );
+}
 
 export default HomePage;
