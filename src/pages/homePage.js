b8b42a9c7bafd41914f9e6fb103c56ea802f1668
ericchecuz <eric.checuz96@gmail.com>
2024-04-18 16:53:11 +0200

    add section projects with some cards and effect.

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -1,6 +1,51 @@
 import React, { useState, useEffect } from "react";
 import { auth } from "../firebase/firebaseConfig";
 import "./styleHome.css";
+import { useInView } from "react-intersection-observer";
+import card1 from "../assets/card1.png";
+import card3 from "../assets/cards2.png";
+import card2 from "../assets/cards3.png";
+import card4 from "../assets/cards4.png";
+
+const ProjectCards = ({ title, description, image }) => {
+  const { ref, inView } = useInView({
+    threshold: 0.5, // Trigger quando il 50% della card è visibile
+  });
+
+  return (
+    <>
+      <div ref={ref} className={`card ${inView ? "fade-in" : "fade-out"}`}>
+        <img className="img-cards" src={card1} />
+        <h3>Project Angular Portfolio</h3>
+        <p>
+          A simple Portfolio made with Angular 16 and my custom components,
+          using a simple design UX/UI.
+        </p>
+      </div>
+      <div ref={ref} className={`card ${inView ? "fade-in" : "fade-out"}`}>
+        <img className="img-cards" src={card2} alt={title} />
+        <h3>Angular User Dashboard</h3>
+        <p>
+          Dashboard made in Angular 16 with implementation of FireBase
+          Database/Auth, to control the users , and more features.
+        </p>
+      </div>
+      <div ref={ref} className={`card ${inView ? "fade-in" : "fade-out"}`}>
+        <img className="img-cards" src={card3} alt={title} />
+        <h3>React Portfolio</h3>
+        <p>
+          A complete portfolio with my projects and tutorial, made with my
+          custom components.
+        </p>
+      </div>
+      <div ref={ref} className={`card ${inView ? "fade-in" : "fade-out"}`}>
+        <img className="img-cards" src={card4} alt={title} />
+        <h3>Coming soon..</h3>
+        <p>{description}</p>
+      </div>
+    </>
+  );
+};
 
 function HomePage() {
   const [transform, setTransform] = useState("");
@@ -19,7 +64,28 @@ function HomePage() {
   const handleMouseLeave = () => {
     setTransform(""); // Resetta la trasformazione quando il mouse lascia il contenitore
   };
-
+  const projects = [
+    {
+      title: "Project 1",
+      description: "Description 1",
+      image: { card1 },
+    },
+    {
+      title: "Project 2",
+      description: "Description 2",
+      image: "../assets/home2.png",
+    },
+    {
+      title: "Project 3",
+      description: "Description 3",
+      image: "path/to/image3.jpg",
+    },
+    {
+      title: "Project 4",
+      description: "Description 4",
+      image: "path/to/image4.jpg",
+    },
+  ];
   return (
     <div>
       <div className="main-container">
@@ -35,7 +101,7 @@ function HomePage() {
             <a className="link"> CONTACT</a>
             <div className="container-theme">
               {" "}
-              <span class="material-symbols-outlined">dark_mode</span>
+              <span className="material-symbols-outlined">dark_mode</span>
             </div>
           </div>
         </header>
@@ -77,6 +143,8 @@ function HomePage() {
         <div className="arrow-container">
           <div className="arrow"></div>
         </div>
+      </div>
+      <div className="container-img2">
         <div className="container-contacts">
           <div className="contacts">
             <div className="container-icons">
@@ -88,9 +156,20 @@ function HomePage() {
             </div>
           </div>
         </div>
-        <div className="text-project">Projects</div>
+        <div className="text-project">Projects</div>{" "}
+        <div className="section-projects">
+          <div
+            className="projects-container"
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "space-around",
+            }}
+          >
+            <ProjectCards />
+          </div>
+        </div>
       </div>
-      <div className="container-img2"></div>
     </div>
   );
 }
