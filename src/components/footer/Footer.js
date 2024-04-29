e58e072ea77ba16edf461b79c72b0ed0eef719aa
ericchecuz <eric.checuz96@gmail.com>
2024-04-29 12:18:17 +0200

    fix footer component

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -7,32 +7,35 @@ const Footer = () => {
       <div className="container">
         <div className="container-about">
           <div className="text-footer">ABOUT ME </div>
-          <p>Telegram </p>
-          <p>Linkedin </p>
-          <p>Instagram </p>
-          <p>Facebook </p>
+          <p className="p-footer">✉ Telegram </p>
+          <p className="p-footer">✉ Linkedin </p>
+          <p className="p-footer">✉ Instagram </p>
+          <p className="p-footer">✉ Facebook </p>
         </div>
 
         <div className="container-community">
           <div className="text-footer">COMMUNITIES </div>
-          <p>Angular </p>
-          <p>React</p>
-          <p>JavaScript </p>
+          <p className="p-footer">☆ Angular </p>
+          <p className="p-footer">☆ React</p>
+          <p className="p-footer">☆ JavaScript </p>
         </div>
         <div className="container-prod-sec">
           <div className="container-product">
             <div className="text-footer">PRODUCTS </div>
-            <p>Front-end Projects </p>
-            <p>Tutorials </p>
+            <p className="p-footer">❤ Front-end Projects </p>
+            <p className="p-footer">❤ Tutorials </p>
           </div>
           <div className="container-section">
             <div className="text-footer">SECTIONS</div>
-            <p>About </p>
-            <p>Works </p>
-            <p>Contact </p>
+            <p className="p-footer">✘ About </p>
+            <p className="p-footer">✘ Works </p>
+            <p className="p-footer">✘ Contact </p>
           </div>
         </div>
-        <p>© 2024 Eric's Device. All rights reserved.</p>
+        <div className="container-footer-image">
+          <div className="container-footer-img"></div>
+          <p>© 2024 Eric's Device. All rights reserved.</p>
+        </div>
       </div>
     </footer>
   );
